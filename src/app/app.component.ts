604728e890bd834454f0dafe1179bf76a3133bd8
vinodkumarborse <vinod.borse@appdirect.com>
2020-07-28 22:48:59 +0530

    Add initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'breaking-bad';
+
+  charactersData = [];
+
+  private url: string = "https://www.breakingbadapi.com/api/characters";
+  constructor( private http: HttpClient) { }
+
+  getData() {
+    this.http.get(this.url)
+    .pipe(
+      map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], customId: key});
+          }
+        }
+        return postsArray;
+      })
+    )
+    .subscribe(data => {
+        this.charactersData = data;
+        console.log(this.charactersData);
+      }
+    );
+  }
 }

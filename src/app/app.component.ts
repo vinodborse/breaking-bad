1deab912d8d82b24eb973571be7758e754cfc9f5
vinodkumarborse <vinod.borse@appdirect.com>
2020-07-29 23:01:44 +0530

    Add images and full functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,17 @@ import { map } from "rxjs/operators";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   charactersData = [];
-
-  private url: string = "https://www.breakingbadapi.com/api/characters";
+  fershPage = true;
+  isSearchChar = false;
+  isLoader = true;
+  private baseUrl: string = "https://www.breakingbadapi.com/api/characters";
   constructor( private http: HttpClient) { }
 
-  getData() {
-    this.http.get(this.url)
+  getAllChars() {
+    this.fershPage = true;
+    this.isLoader = false;
+    this.http.get(this.baseUrl)
     .pipe(
       map(responseData => {
         const postsArray = [];
@@ -28,6 +31,35 @@ export class AppComponent {
       })
     )
     .subscribe(data => {
+        this.fershPage = false; 
+        this.isLoader = true;
+        this.charactersData = data;
+        console.log(this.charactersData);
+
+      }
+    );
+  }
+
+  searchChar() {
+    this.isSearchChar = !this.isSearchChar;
+  }
+
+  getChar(charName: string) {
+    console.log(charName);
+    this.http.get(this.baseUrl + "?name=" + charName)
+    .pipe(
+      map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], customId: key});
+          }
+        }
+        return postsArray;
+      })
+    )
+    .subscribe(data => {
+        this.fershPage = false; 
         this.charactersData = data;
         console.log(this.charactersData);
       }
